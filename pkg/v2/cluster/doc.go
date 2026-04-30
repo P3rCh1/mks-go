be5af2b643fa479fe05aa0e20fffcd590b526f95
P3rCh1 <thep3rch1@gmail.com>
2026-04-30 14:50:39 +0300

    doc comments

--- pkg/v2/cluster/doc.go
+++ pkg/v2/cluster/doc.go
@@ -9,29 +9,5 @@ Example of getting a single cluster referenced by its id
 		log.Fatal(err)
 	}
 	fmt.Printf("%+v\n", mksCluster)
-
-Example of creating a new cluster
-
-	clusterBody := &mksclient.ClusterCreateStruct{
-		Name:        "test-cluster",
-		KubeVersion: "1.28",
-		Region:      new("ru-1"),
-		Nodegroups:  []*mksclient.NodegroupCreateStruct{
-			{
-				Name:             "test-nodegroup",
-				Count:            1,
-				CPUs:             new(2),
-				RAM:              new(4096),
-				VolumeSize:       new(50),
-				VolumeType:       new("standard"),
-				AvailabilityZone: new("ru-3a"),
-			},
-		},
-	}
-	mksCluster, err := cluster.Create(ctx, client, clusterBody)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%+v\n", mksCluster)
 */
 package cluster
--- pkg/v2/mksclient/errors.go
+++ pkg/v2/mksclient/errors.go
@@ -1,23 +1,22 @@
 package mksclient
 
-import "fmt"
-
-// errGotHTTPStatusCodeFmt is an error message format for unexpected HTTP status codes.
-const errGotHTTPStatusCodeFmt = "mks-go: got the %d status code with message: %s"
-
+// MKSError is the custom error type for mks-go API errors.
 type MKSError struct {
 	StatusCode int
 	Message    string
 }
 
+// Status returns the HTTP status code of the error.
 func (e *MKSError) Status() int {
 	return e.StatusCode
 }
 
+// Error returns the error message.
 func (e *MKSError) Error() string {
 	return e.Message
 }
 
+// ToMKSError converts to an MKSError with the given status code.
 func (e *GenericError) ToMKSError(statusCode int) *MKSError {
 	return &MKSError{
 		StatusCode: statusCode,
@@ -25,6 +24,7 @@ func (e *GenericError) ToMKSError(statusCode int) *MKSError {
 	}
 }
 
+// ToMKSError converts to an MKSError with the given status code.
 func (e *GenericNotFoundError) ToMKSError(statusCode int) *MKSError {
 	return &MKSError{
 		StatusCode: statusCode,
@@ -32,10 +32,13 @@ func (e *GenericNotFoundError) ToMKSError(statusCode int) *MKSError {
 	}
 }
 
+// APIError is the interface for convertable to MKSError types.
 type APIError interface {
 	ToMKSError(statusCode int) *MKSError
 }
 
+// HandleAPIErrors processes a list of possible API errors and returns the first one found.
+// If no API error is found, it returns a generic MKSError using the provided status code and message.
 func HandleAPIErrors(statusCode int, statusMsg string, errors ...APIError) error {
 	for _, err := range errors {
 		if err != nil {
@@ -43,5 +46,8 @@ func HandleAPIErrors(statusCode int, statusMsg string, errors ...APIError) error
 		}
 	}
 
-	return fmt.Errorf(errGotHTTPStatusCodeFmt, statusCode, statusMsg)
+	return &MKSError{
+		StatusCode: statusCode,
+		Message:    statusMsg,
+	}
 }
