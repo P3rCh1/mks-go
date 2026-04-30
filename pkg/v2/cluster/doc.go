59003923a7f64e7b11652acc84b79f004b7a826c
P3rCh1 <thep3rch1@gmail.com>
2026-04-30 14:20:04 +0300

    update doc file

--- pkg/v2/cluster/doc.go
+++ pkg/v2/cluster/doc.go
@@ -8,17 +8,7 @@ Example of getting a single cluster referenced by its id
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%+v\n", mksCluster.JSON200)
-
-Example of getting all clusters
-
-	mksClusters, err := cluster.List(ctx, client)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, mksCluster := range mksClusters.JSON200.Items {
-		fmt.Printf("%+v\n", mksCluster)
-	}
+	fmt.Printf("%+v\n", mksCluster)
 
 Example of creating a new cluster
 
@@ -42,76 +32,6 @@ Example of creating a new cluster
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%+v\n", mksCluster.JSON201)
-
-Example of updating an existing cluster
-
-	updateOpts := &mksclient.ClusterUpdateStruct{
-		MaintenanceWindowStart: new("07:00:00"),
-		KubernetesOptions: &mksclient.KubernetesOptions{
-			EnablePodSecurityPolicy: new(false),
-			FeatureGates: &[]string{
-				"TTLAfterFinished",
-			},
-			AdmissionControllers: &[]string{
-				"NamespaceLifecycle",
-			},
-		},
-	}
-	mksCluster, err := cluster.Update(ctx, client, clusterID, updateOpts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%+v\n", mksCluster.JSON200)
-
-Example of deleting a single cluster
-
-	resp, err := cluster.Delete(ctx, client, clusterID)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-Example of getting a kubeconfig referenced by cluster id
-
-	kubeconfig, err := cluster.GetKubeconfig(ctx, client, clusterID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Print(string(kubeconfig.Body))
-
-Example of getting fields from Kubeconfig referenced by cluster id
-
-	parsedKubeconfig, err := cluster.GetParsedKubeconfig(ctx, client, clusterID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Server IP:", parsedKubeconfig.Server)
-	fmt.Println("Cluster CA:", parsedKubeconfig.ClusterCA)
-	fmt.Println("Client cert:", parsedKubeconfig.ClientCert)
-	fmt.Println("Client key:", parsedKubeconfig.ClientKey)
-	fmt.Println("Raw kubeconfig:", parsedKubeconfig.KubeconfigRaw)
-
-Example of rotating certificates by cluster id
-
-	resp, err := cluster.RotateCerts(ctx, client, clusterID)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-Example of upgrading Kubernetes patch version by cluster id
-
-	mksCluster, err := cluster.UpgradePatchVersion(ctx, client, clusterID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%+v\n", mksCluster.JSON200)
-
-Example of upgrading Kubernetes minor version by cluster id
-
-	mksCluster, err := cluster.UpgradeMinorVersion(ctx, client, clusterID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%+v\n", mksCluster.JSON200)
+	fmt.Printf("%+v\n", mksCluster)
 */
 package cluster
